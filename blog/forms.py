419a80990b53620d141b26a6914aa9387a33849d
mojtaba rakhisi <mojtaba.rakhisi@gmail.com>
2022-08-13 19:06:39 +0430

    feat: add comment views and model to app

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from dataclasses import fields
+from .models import Comment
+from django import forms
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name' , 'email' , 'body')
